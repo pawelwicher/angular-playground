3fe4101d44602d01f104d2f7a71a09493febd13d
wichu <pawel.wicher@op.pl>
2025-06-14 07:42:23 +0200

    foo

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
@@ -22,4 +23,5 @@ describe('App', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-playground');
   });
+
 });
